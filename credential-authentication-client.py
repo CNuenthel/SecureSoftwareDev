57b5cf778fe5e284282552a26eb8e87ff7e322b9
Cody Nuenthel <89996557+CNuenthel@users.noreply.github.com>
2025-01-11 17:37:12 -0600

    Update credential-authentication-client.py

--- credential-authentication-client.py
+++ credential-authentication-client.py
@@ -3,7 +3,7 @@ import requests
 # Example for obtaining a token (this depends on the API)
 auth_url = 'http://127.0.0.1:5000/api/data'
 
-# Send a POST request to get the token
+# Send a POST request to get the token, these creds are fake, don't be stupid
 auth_response = requests.get(auth_url, auth=("ndus-cody", "adaeaea0-6b5c-4d7a-948d-4fa86373a522"))
 
 
